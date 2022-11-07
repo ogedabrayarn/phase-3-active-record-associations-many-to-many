ce21a6d197121b584d7dd34a74139b8690d3f2d8
Brayarn Ogeda <brayarn.oduor@student.moringaschool.com>
2022-11-07 07:15:07 +0300

    completed associations many-to-many >> rake db:test:prepare

--- db/migrate/20221107020359_add_user_id_to_reviews.rb
+++ db/migrate/20221107020359_add_user_id_to_reviews.rb
@@ -1,5 +1,5 @@
 class AddUserIdToReviews < ActiveRecord::Migration[6.1]
   def change
-    add_column :reviews, :user_id, :integer
+    add_column :reviews, :user_id, :integer #add a user_id column to reviews table
   end
 end
